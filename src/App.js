0eedc16c2fb4fc36bfeff942ab2a2b75e5942f04
thomaslty <thomaslty16@gmail.com>
2021-11-03 01:10:08 +0800

    re-render problem?

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
         </button>
       </div>
       <div className="App-progress-bar">
-        <Line percent={percentage} strokeWidth="4" strokeColor="#D3D3D3" />
+        <Line percent={"12"} strokeWidth="4" strokeColor="#D3D3D3" />
       </div>
     </div>
   );
