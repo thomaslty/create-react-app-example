5cb241c62c92e3b39890c6302d202cd5553d6787
thomaslty <thomaslty16@gmail.com>
2021-11-03 01:02:29 +0800

    progress bar test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from "react";
-// import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { Line } from "rc-progress";
 
 const App = () => {
+  const [percentage, setPercentage] = useState(0);
   return (
     <div className="App">
-      <div>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
+      <div className="App-intro-container">
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </div>
+      <div className="App-button">
+        <button
+          type="button"
+          className="btn1"
+          onclick={setPercentage(
+            percentage + 10 >= 100 ? percentage + 10 : percentage
+          )}
+        >
+          +10
+        </button>
+      </div>
+      <div className="App-progress-bar">
+        <Line percent={percentage} strokeWidth="4" strokeColor="#D3D3D3" />
       </div>
     </div>
   );
