7de87a5ec8a799f44742f007ab410342cc516f3e
thomaslty <thomaslty16@gmail.com>
2021-11-03 00:20:31 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import bg from "../asset/background.png";
 
 const App = () => {
+  const [testMsg, setTestMsg] = useState("testmsg");
+  useEffect(() => {});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+      <div>
+        <p>{testMsg}</p>
+        <img src={bg} />
+      </div>
+    </>
   );
 };
 
